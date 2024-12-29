f526852d56af578ffe9f2e4d2ee7c528f2b3d135
Wienerman <Nem56@NEMO-WINDOWS>
2024-12-30 00:27:54 +0100

    Forgot to add whoopsie

---
+++ VoxelGame/Graphics/MeshData/Mesh.cs
@@ -0,0 +1,11 @@
+﻿using Graphics.GpuMemory;
+
+namespace Graphics
+{
+    public struct Mesh<T> where T : struct
+    {
+        public GpuArrayBuffer<T> VertexBuffer { get; init; }
+        public GpuArrayBuffer<uint> IndexBuffer { get; init; }
+        public GpuBufferStructure BufferStructure { get; init; }
+    }
+}
---
+++ VoxelGame/Utils/MatrixHelper.cs
@@ -0,0 +1,23 @@
+﻿using OpenTK.Mathematics;
+
+namespace Utils
+{
+    public static class MatrixHelper
+    {
+        public static Matrix3 Lerp(Matrix3 from, Matrix3 to, float t)
+        {
+            t = MathHelper.Clamp(t, 0f, 1f);
+
+            return new Matrix3(
+                Lerp(from.M11, to.M11, t), Lerp(from.M12, to.M12, t), Lerp(from.M13, to.M13, t),
+                Lerp(from.M21, to.M21, t), Lerp(from.M22, to.M22, t), Lerp(from.M23, to.M23, t),
+                Lerp(from.M31, to.M31, t), Lerp(from.M32, to.M32, t), Lerp(from.M33, to.M33, t)
+            );
+        }
+
+        public static float Lerp(float a, float b, float t)
+        {
+            return a + (b - a) * t;
+        }
+    }
+}
